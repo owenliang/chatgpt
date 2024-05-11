ecc28ea9415079d76540ab182c776d245b15fc72
owenliang <owenliang1990@gmail.com>
2024-05-11 23:49:31 +0800

    up

---
+++ emb.py
@@ -0,0 +1,21 @@
+from torch import nn 
+import torch 
+import math 
+
+class EmbeddingWithPosition(nn.Module):
+    def __init__(self,vocab_size,dim,seq_max_len=5000):
+        super().__init__()
+        
+        self.seq_emb=nn.Embedding(vocab_size,dim)
+
+        position_idx=torch.arange(0,seq_max_len,dtype=torch.float).unsqueeze(-1)
+        position_emb_fill=position_idx*torch.exp(-torch.arange(0,dim,2)*math.log(10000.0)/dim)
+        pos_encoding=torch.zeros(seq_max_len,dim)
+        pos_encoding[:,0::2]=torch.sin(position_emb_fill)
+        pos_encoding[:,1::2]=torch.cos(position_emb_fill)
+        self.register_buffer('pos_encoding',pos_encoding) 
+
+    def forward(self,x):    # x: (batch_size,seq_len)
+        x=self.seq_emb(x)   # x: (batch_size,seq_len,dim)
+        x=x+self.pos_encoding.unsqueeze(0)[:,:x.size()[1],:] # x: (batch_size,seq_len,dim)
+        return x
--- gpt.py
+++ gpt.py
@@ -1,21 +1,44 @@
-from torch import Module
 from torch import nn
+import torch 
+from emb import EmbeddingWithPosition
 
-class GPT(Module):
+class GPT(nn.Module):
     def __init__(self,d_model,nhead,feedforward,vocab_size):
         super().__init__()
         
         # positional encoding...
-        
-        # vocab embedding
-        self.emb=nn.Embedding(num_embeddings=vocab_size,embedding_dim=d_model)
+        self.emb=EmbeddingWithPosition(vocab_size=vocab_size,dim=d_model)
         
         # decoder-only transformer (self-attention)
-        self.dec_layers=nn.ModuleList([
+        self.dec_blocks=nn.ModuleList([
             nn.TransformerEncoderLayer(d_model=d_model,nhead=nhead,dim_feedforward=feedforward,batch_first=True) for _ in range(5)
         ])
         # next token probability
         self.prob_linear=nn.Linear(d_model,vocab_size)
     
-    def forward(self,batch_x,attention_mask):
-        pass
+    def forward(self,x,padding_mask): # x:(batch,seq)
+        # 注意力遮挡
+        src_mask=torch.triu(torch.ones(x.size()[1],x.size()[1]),diagonal=1)
+        # embedding
+        x=self.emb(x)
+        # decoder
+        for block in self.dec_blocks:
+            x=block(x,src_mask=src_mask,src_key_padding_mask=padding_mask)
+        # logits
+        logits=self.prob_linear(x)
+        return logits
+
+if __name__=='__main__':
+    # 分词器
+    from bpe import BPETokenizer
+    tokenizer=BPETokenizer()
+    tokenizer.load('tokenizer.bin')
+    
+    # 模拟输入
+    x=torch.randint(0,tokenizer.vocab_size(),(5,30))
+    padding=torch.zeros(5,30)
+    
+    # GPT模型
+    gpt=GPT(d_model=64,nhead=2,feedforward=128,vocab_size=tokenizer.vocab_size())
+    y=gpt(x,padding)
+    print(y.shape)
